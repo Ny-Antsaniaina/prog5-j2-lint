3f91fba4255092162e1e78d9ab0450b236d95dd6
Ny-Antsaniaina <hei.nyantsaniaina@gmail.com>
2025-11-21 13:07:05 +0100

    chore; add isOpen

--- src/wallet.py
+++ src/wallet.py
@@ -1,17 +1,17 @@
-class Wallet():
-    def __init__(self , balance , size , open):
+class Wallet:
+    def __init__(self, balance, size, isOpen):
         self.balance = balance
         self.size = size
-        self.close = open
-        
-    def addMoney(self , amount):
+        self.isOpen = isOpen
+
+    def addMoney(self, amount):
         self.balance += amount
-        
+
     def checkMoney(self):
-        print(f'you have a {self.balance} $' )
-        
+        print(f'you have a {self.balance} $')
+
     def getMoney(self, amount):
         if self.balance >= amount:
             self.balance -= amount
         else:
-            print("money not enougth")
+            print("money not enough")
