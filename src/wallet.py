17da7c51e668c0289d3c6c128d658b17f2645a99
Ny-Antsaniaina <hei.nyantsaniaina@gmail.com>
2025-11-21 12:56:01 +0100

    chore: add few methodes

--- src/wallet.py
+++ src/wallet.py
@@ -1,5 +1,17 @@
 class Wallet():
-    def __init__(self , balance):
+    def __init__(self , balance , size , open):
         self.balance = balance
+        self.size = size
+        self.close = open
+        
     def addMoney(self , amount):
         self.balance += amount
+        
+    def checkMoney(self):
+        print(f'you have a {self.balance} $' )
+        
+    def getMoney(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("money not enougth")
