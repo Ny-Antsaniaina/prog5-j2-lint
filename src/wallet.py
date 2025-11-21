49579243672713c727352181a82dfc1aa1d5d138
Ny-Antsaniaina <hei.nyantsaniaina@gmail.com>
2025-11-21 13:17:22 +0100

    chore: add isOpen on few method

--- src/wallet.py
+++ src/wallet.py
@@ -1,17 +1,20 @@
 class Wallet:
-    def __init__(self, balance, size, isOpen):
+    def __init__(self, balance, size, isOpen , color):
         self.balance = balance
         self.size = size
         self.isOpen = isOpen
+        self.color = color
 
     def addMoney(self, amount):
         self.balance += amount
 
     def checkMoney(self):
+        self.isOpen = True
         print(f'you have a {self.balance} $')
 
     def getMoney(self, amount):
         if self.balance >= amount:
+            self.isOpen = True
             self.balance -= amount
         else:
             print("money not enough")
